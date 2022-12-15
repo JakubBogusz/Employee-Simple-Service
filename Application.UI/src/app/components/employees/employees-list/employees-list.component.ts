1959423142aa7962c325ab65c43b4a746a538c62
jbogusz <jakub.bogusz@unitygroup.com>
2022-12-15 20:31:30 +0100

    clean solution

--- Application.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ Application.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -8,38 +9,18 @@ import { Employee } from 'src/app/models/employee';
 })
 export class EmployeesListComponent implements OnInit {
 
-  employees: Employee[] = [
-    {
-      Id: '0a906621-1e06-480c-9afb-7f92c1acbb59',
-      FirstName: 'Michael',
-      LastName: 'Jordan',
-      StreetName: 'Mair Avenue',
-      HouseNumber: '76',
-      ApartmentNumber: 12,
-      PostalCode: '30-202',
-      Town: 'Warsaw',
-      PhoneNumber: 888234994,
-      DateOfBirth: new Date("1980-01-16"),
-      Age: 42
-    },
-    {
-      Id: '9f2c3263-15f4-4e9e-9fd3-465be7cf4d7e',
-      FirstName: 'Lebron',
-      LastName: 'James',
-      StreetName: 'CA Street',
-      HouseNumber: '156C',
-      ApartmentNumber: null,
-      PostalCode: '45-208',
-      Town: 'Warsaw',
-      PhoneNumber: 228235591,
-      DateOfBirth: new Date("1991-02-10"),
-      Age: 31
-    }
-  ];
-  constructor() { }
+  employees: Employee[] = [];
+  constructor(private employeesService: EmployeesService) { }
 
   ngOnInit(): void {
-
+    this.employeesService.getAllEmployees()
+    .subscribe({
+      next: (employees) => {
+        console.log(employees);
+      },
+      error: (response) => {
+          console.log(this.employees);
+      }
+    })
   }
-
 }
